8d294d9b89eec54afcdfa7c38fb446dc1612a09c
Chris Vasterling <chrisvasterling@gmail.com>
2023-07-27 15:43:10 -0500

    Update scripts & message

--- components/App.tsx
+++ components/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App (props: any): JSX.Element {
   return (
     <>
-      <h1>Hello</h1>
+      <h1>⚠️Under Construction⚠️</h1>
       <span>- Chris Vasterling</span>
     </>
   )
